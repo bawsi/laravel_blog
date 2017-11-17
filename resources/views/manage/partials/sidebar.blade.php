2fa4c6c4f5f0d820301bfe9ad6c53ba3281cfe8d
Blaž <petekblaz94@gmail.com>
2017-11-17 19:04:43 +0100

    Manage sidebar now has dinamically added "is-active" classes

--- resources/views/manage/partials/sidebar.blade.php
+++ resources/views/manage/partials/sidebar.blade.php
@@ -2,15 +2,16 @@
    
     <p class="menu-label">General</p>
     <ul class="menu-list">
-        <li><a href="{{ route('manage.dashboard') }}" class="is-active">Dashboard</a></li>
+        <li><a href="{{ route('manage.dashboard') }}" {!! request()->is('manage/dashboard') ? 'class="is-active"' : '' !!}>Dashboard</a></li>
     </ul>
 
     <p class="menu-label">Administration</p>
     <ul class="menu-list">
 
-        <li><a href="{{ route('users.index') }}">Users</a></li>
-        <li><a href="{{ route('roles.index') }}">Roles</a></li>
-        <li><a href="{{ route('posts.index') }}">Posts</a></li>
-        <li><a href="{{ route('categories.index') }}">Categories</a></li>
+        <li><a href="{{ route('users.index') }}" {!! request()->is('manage/users') ? 'class="is-active"' : '' !!}>Users</a></li>
+        <li><a href="{{ route('roles.index') }}" {!! request()->is('manage/roles') ? 'class="is-active"' : '' !!}>Roles</a></li>
+        <li><a href="{{ route('posts.index') }}" {!! request()->is('manage/posts') ? 'class="is-active"' : '' !!} 
+            {!! request()->is('manage/posts/create') ? 'class="is-active"' : '' !!}>Posts</a></li>
+        <li><a href="{{ route('categories.index') }}" {!! request()->is('manage/categories') ? 'class="is-active"' : '' !!}>Categories</a></li>
     </ul>
 </aside>
