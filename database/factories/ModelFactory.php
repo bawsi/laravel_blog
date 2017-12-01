2e2dc494dd8f6043d51ecdad9d490399a4147c1d
Blaž <petekblaz94@gmail.com>
2017-12-01 15:13:56 +0100

    Setup seeder and factory for User, Post, Category and Role

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,14 +11,29 @@
 |
 */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+// User
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name' 			 => $faker->name,
+        'email' 		 => $faker->unique()->safeEmail,
+        'password' 		 => $password ?: $password = bcrypt('12345'),
         'remember_token' => str_random(10),
+    ];
+});
+
+
+
+// Post
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' 		 => $faker->words(7, true),
+        'body' 			 => $faker->paragraph(28),
+        'thumbnail_path' => 'http://via.placeholder.com/450x200',
+        'header_path'    => 'http://via.placeholder.com/2560x460', 
+        'user_id' 		 => App\User::all()->random()->id,
+        'category_id' 	 => App\Category::all()->random()->id,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+    	// Making main admin role
+    	DB::table('roles')->insert([
+            'name' => 'Admin',
+        ]);
+
+        // Making main category
+    	DB::table('categories')->insert([
+            'name' => 'Other',
+        ]);
+
+    	// Making main admin account for testing
+    	DB::table('users')->insert([
+            'name' 		=> 'Blaz',
+            'email' 	=> 'blaz@example.com',
+            'password'  => bcrypt('12345'),
+            'role_id'   => 1
+        ]);
+
+    	// Making random users and posts
+        factory(App\User::class, 10)->create();
+        factory(App\Post::class, 35)->create();
     }
 }
