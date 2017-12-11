ffa351e4e418678569afdef3b9af80ff8d44f925
Blaž <petekblaz94@gmail.com>
2017-12-11 19:08:10 +0100

    If user has no posts, return string saying so

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -14,9 +14,11 @@ class ManageController extends Controller
 
     public function dashboard()
     {
-    	$mostActiveCategoryId = auth()->user()->posts->groupBy('category_id')->max()->first()->category_id;
-    	$categoryName = Category::find($mostActiveCategoryId)->name;
+    	$mostActiveCategoryId = auth()->user()->posts->count() ? auth()->user()->posts->groupBy('category_id')->max()->first()->category_id : 0;
+    	
+        $categoryName = $mostActiveCategoryId ? Category::find($mostActiveCategoryId)->name : 'None yet';
+        $latestArticleCreatedAt = auth()->user()->posts->count() ? auth()->user()->posts->first()->created_at->toFormattedDateString() : 'No article yet published';
 
-    	return view('manage.dashboard')->with(compact('categoryName'));
+    	return view('manage.dashboard')->with(compact('categoryName', 'latestArticleCreatedAt'));
     }
 }
