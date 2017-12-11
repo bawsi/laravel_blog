32647148b8e2aaaa4566f4fc102a2adda17bbeb0
Blaž <petekblaz94@gmail.com>
2017-12-11 19:17:19 +0100

    Renaming some labels, since their "for" attribute was wrong

--- resources/views/users/partials/edit-user-modal.blade.php
+++ resources/views/users/partials/edit-user-modal.blade.php
@@ -9,11 +9,11 @@
 			
 			<h1 class="title is-3">Updating an existing user</h1>
 			<h6 class="subtitle is-6">Leave fields you dont wish to update, empty.</h6>
-			<label for="new-posts-category">New name</label>
+			<label for="name">New name</label>
 			<input class="input" type="text" name="name" minlength="2" maxlength="15" placeholder="Users name here">
-			<label for="new-posts-category">New email</label>
+			<label for="email">New email</label>
 			<input class="input" type="email" name="email" minlength="2" maxlength="25" placeholder="Email here">
-			<label for="new-posts-category">New role</label>
+			<label for="role_id">New role</label>
 			
 			<select class="select" name="role_id">
 				<option selected disabled value>Keep the same</option>
@@ -22,9 +22,9 @@
 				@endforeach
 			</select>
 
-			<label for="new-posts-category">New password</label>
+			<label for="password">New password</label>
 			<input class="input" type="password" name="password" minlength="2" maxlength="35" placeholder="Password here">
-			<label for="new-posts-category">Password confirmation</label>
+			<label for="password_confirmation">Password confirmation</label>
 			<input class="input" type="password" name="password_confirmation" minlength="2" maxlength="35" placeholder="Password confirmation here">
 			<button class="button is-info is-pulled-right" type="submit">Update user</button>
 			<button type="button" class="button is-pulled-right close-modal">Cancel</button>
