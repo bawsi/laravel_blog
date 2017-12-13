5c24b1d913e85152b688de170bd0f380bb449c70
Blaž <petekblaz94@gmail.com>
2017-12-13 18:22:35 +0100

    Implemented edit and delete butotns on articles.show view
    They only show if user is logged in + is an author of article or admin (id of 1)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,6 +13,17 @@
 
 		<div class="container "> {{-- content class allows normal usage of lists, blockquotes,... --}}
 			<article>
+				
+				{{-- Edit and delete buttons --}}
+				@if (Auth::check())
+					@if ($post->user_id == Auth::user()->id || Auth::user()->role_id == 1)
+						<div class="buttons">
+							<a href="{{ route('posts.edit', $post->id) }}" class="button is-info m-t-10" type="submit"><span class="fa fa-pencil-square-o m-r-5"></span> Edit Article</a>
+							<a href="{{ route('posts.destroy', $post->id) }}" class="button is-danger m-t-10" type="submit"><span class="fa fa-trash-o m-r-5"></span> Delete Article</a>
+						</div>
+					@endif
+				@endif
+
 				<p class="body">{!! $post->body !!}</p>
 			</article>
 		</div>
