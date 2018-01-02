9db9f783b72e4589bba0f2d9c9b9cfbf2cca1e5b
Blaž <petekblaz94@gmail.com>
2018-01-02 11:55:09 +0100

    Now linking to css, in template via asset() function

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
 <!-- Font awesome CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- Main stylesheet -->
-<link rel="stylesheet" type="text/css" href="/css/app.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 <!-- Animate.css -->
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 <!-- fonts -->
