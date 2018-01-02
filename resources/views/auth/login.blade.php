c2971ab6f3f01470bebb52354d7d6629283620dd
Blaž <petekblaz94@gmail.com>
2018-01-02 12:58:49 +0100

    Login page styling changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-	<div class="container">
+	<div class="container login-container">
+			<h1 class="title">Login</h1>
+			<hr>
 		<form action="{{ route('auth.login') }}" method="POST">
 			{{ csrf_field() }}
-
 			<label class="label">Email</label>
 			<input class="input" type="email" name="email" placeholder="Email here">
-			<label class="label">Password</label>
+			<label class="label" id="passLabel">Password</label>
 			<input class="input" type="password" name="password" placeholder="Password here">
-			<button class="button is-info" type="submit">Login</button>
+			<button class="button is-info is-fullwidth" type="submit">Login</button>
 		</form>
+			
 	</div>
-
-{{ bcrypt("12345") }}	
 
 @endsection
